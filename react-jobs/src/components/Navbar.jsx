f35541b226bf71bb825eaa5bd847ffd00e7256f1
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-05-19 17:06:36 +0200

    change active state styles with NavLink

--- react-jobs/src/components/Navbar.jsx
+++ react-jobs/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const listClass = ({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <>
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -24,21 +26,22 @@ const Navbar = () => {
             </a>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <Link
+                <NavLink
                   to="/"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home
-                  </Link>
-                  <Link
+                  className={ listClass }
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
                   to="/jobs"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={ listClass }
                   >Jobs
-                  </Link>
-                <Link
+                  </NavLink>
+                <NavLink
                   to="/add-job"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={ listClass }
                   >Add Job
-                  </Link>
+                  </NavLink>
               </div>
             </div>
           </div>
